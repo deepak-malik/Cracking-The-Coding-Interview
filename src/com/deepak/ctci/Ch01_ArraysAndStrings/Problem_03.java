24caae5e1fb4e3fefb707ac2fee5d92ce753a60d
Deepak Malik <deepak.malik174266@gmail.com>
2017-01-19 22:49:53 -0800

    Problem 03 - Ch 01

---
+++ src/com/deepak/ctci/Ch01_ArraysAndStrings/Problem_03.java
@@ -0,0 +1,50 @@
+/**
+ * Cracking-The-Coding-Interview
+ * Problem_03.java
+ */
+package com.deepak.ctci.Ch01_ArraysAndStrings;
+
+/**
+ * Write a method to replace all spaces with the string "%20".
+ * You may assume that the string has sufficient space at the end,
+ * to hold additional characters, and you are given the true length
+ * of the string.
+ * 
+ * ex. Input : "Mr John Smith     ", 13
+ * 	   Output : "Mr%20John%20Smith"
+ * 
+ * @author Deepak
+ */
+public class Problem_03 {
+
+	/**
+	 * Method to replace spaces in a string with special character's (Already given)
+	 * True length is also passed in the method
+	 * 
+	 * @param input
+	 * @param trueLength
+	 * @return {@link String}
+	 */
+	public static String replaceSpacesWithSpecialCharacter(String input, int trueLength) {
+		if (input == null || input == "") {
+			return null;
+		}
+		/* Do a substring on the input. This is to make sure, we
+		 * only deal with the string that we need */
+		input = input.substring(0, trueLength);
+		String replacement = "%20";
+		StringBuilder builder = new StringBuilder();
+		/* Convert to character array and loop through each character, 
+		 * if character is space, append the replacement text else append character */
+		char[] charArray = input.toCharArray();
+		for (char c : charArray) {
+			if (c != ' ') {
+				builder.append(c);
+			} else {
+				builder.append(replacement);
+			}
+		}
+		return builder.toString();
+	}
+
+}
---
+++ test/com/deepak/ctci/Ch01_ArraysAndStrings/Problem_03_Test.java
@@ -0,0 +1,29 @@
+/**
+ * Cracking-The-Coding-Interview
+ * Problem_03_Test.java
+ */
+package com.deepak.ctci.Ch01_ArraysAndStrings;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test cases for Problem 03
+ * 
+ * @author Deepak
+ */
+public class Problem_03_Test {
+
+	/**
+	 * Test case to replace spaces with a special character
+	 */
+	@Test
+	public void testReplaceSpacesWithSpecialCharacter() {
+		Assert.assertEquals(Problem_03.replaceSpacesWithSpecialCharacter("Mr John Smith ", 13), "Mr%20John%20Smith");
+		Assert.assertEquals(Problem_03.replaceSpacesWithSpecialCharacter("Mr John Smith ", 14), "Mr%20John%20Smith%20");
+		Assert.assertEquals(Problem_03.replaceSpacesWithSpecialCharacter("Mr John Smith ", 9), "Mr%20John%20S");
+		Assert.assertEquals(Problem_03.replaceSpacesWithSpecialCharacter("", 3), null);
+		Assert.assertEquals(Problem_03.replaceSpacesWithSpecialCharacter(null, 3), null);
+	}
+
+}
