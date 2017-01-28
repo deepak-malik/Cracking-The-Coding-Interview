7ca399613c6e00682fc79f41f8ef059b0a81b0f5
Deepak Malik <deepak.malik174266@gmail.com>
2017-01-28 14:23:12 -0800

    Problem 04 - Ch 01

--- src/com/deepak/ctci/Ch01_ArraysAndStrings/Problem_04.java
+++ src/com/deepak/ctci/Ch01_ArraysAndStrings/Problem_04.java
@@ -4,6 +4,10 @@
  */
 package com.deepak.ctci.Ch01_ArraysAndStrings;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 /**
  * <br> Problem Statement :
  * 
@@ -21,5 +25,48 @@ package com.deepak.ctci.Ch01_ArraysAndStrings;
  * @author Deepak
  */
 public class Problem_04 {
+
+	/**
+	 * Method to check if a given string is a palindrome permutation
+	 * - A string will be considered as a palindrome permutation, 
+	 * if it does not have more then one odd character count
+	 * 
+	 * @param input
+	 * @return {@link boolean}
+	 */
+	public static boolean isPalindromePermutation(String input) {
+		/* If input is null, no further processing */
+		if (input == null) {
+			return false;
+		}
+		/* Counter for characters who have odd count */
+		int oddCount = 0;
+		/* Converting to lower case to avoid comparison issues */
+		input = input.toLowerCase();
+		/* Map to hold count of each character */
+		Map<Character, Integer> countMap = new HashMap<>();
+		for (int i = 0; i < input.length(); i++) {
+			/* Current character that is being evaluated */
+			char currentChar = input.charAt(i);
+			/* If it is a letter or digit, check for existence in map 
+			 * and update the count accordingly */
+			if (Character.isLetter(currentChar) || Character.isDigit(currentChar)) {
+				if (countMap.containsKey(currentChar)) {
+					countMap.put(currentChar, countMap.get(currentChar) + 1);
+				} else {
+					countMap.put(currentChar, 1);
+				}
+			}
+		}
+		/* Loop through the entry set and find how many characters we have
+		 * which has odd count */
+		for (Entry<Character, Integer> key : countMap.entrySet()) {
+			if (key.getValue() % 2 != 0) {
+				oddCount++;
+			}
+		}
+		/* If it is more then 1, return false */
+		return oddCount <= 1;
+	}
 
 }
---
+++ test/com/deepak/ctci/Ch01_ArraysAndStrings/Problem_04_Test.java
@@ -0,0 +1,30 @@
+/**
+ * Cracking-The-Coding-Interview
+ * Problem_04_Test.java
+ */
+package com.deepak.ctci.Ch01_ArraysAndStrings;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test cases for problem 04
+ * 
+ * @author Deepak
+ */
+public class Problem_04_Test {
+
+	/**
+	 * Test case to check palindrome permutation
+	 */
+	@Test
+	public void testPalindromePermutation() {
+		Assert.assertTrue(Problem_04.isPalindromePermutation("Tact Coa"));
+		Assert.assertTrue(Problem_04.isPalindromePermutation("Deepak Malik pmli"));
+		Assert.assertFalse(Problem_04.isPalindromePermutation("Deepak Malik"));
+		Assert.assertFalse(Problem_04.isPalindromePermutation(null));
+		Assert.assertTrue(Problem_04.isPalindromePermutation("pop 1212"));
+		Assert.assertFalse(Problem_04.isPalindromePermutation("1234 1256"));
+	}
+
+}
